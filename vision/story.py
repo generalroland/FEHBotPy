05c93b4cba53cc1e39d651647efab178839d2aa0
David Roland <generalrolandxiii@yahoo.com>
2023-12-04 17:01:52 +0700

    bot

--- vision/story.py
+++ vision/story.py
@@ -33,10 +33,22 @@ class Story:
         self.patterns_direction = list()
         self.x = self.bounding_box.get('left') + self.bounding_box.get('width') - 5
         self.y = self.bounding_box.get('top') + 5
+        self.quest_bounding_box = {
+            "top": 292,
+            "left": self.bounding_box.get("left"),
+            "width": 120,
+            "height": 620,
+        }
 
     async def screenshot(self):
+        self.sct = mss()
         self.sct_original = numpy.asarray(self.sct.grab(self.bounding_box))
         self.sct_img = cv2.cvtColor(self.sct_original, cv2.COLOR_BGRA2GRAY)
+
+    async def get_quest_screenshot(self):
+        sct = mss()
+        _img = numpy.asarray(sct.grab(self.quest_bounding_box))
+        return cv2.cvtColor(_img, cv2.COLOR_BGRA2GRAY)
 
     async def load_patterns(self, _list: list):
         _folder = str()
@@ -85,7 +97,7 @@ class Story:
                         x = match_locations[-1][0] + w / 2 + self.bounding_box.get('left')
                         y = match_locations[-1][1] + h / 2 + self.bounding_box.get('top')
                         self.mouse.set_position_and_left_click(x, y)
-                        # print(f"{max_val} click {prio_pattern.type} {prio_pattern.name}")
+                        print(f"{max_val} click {prio_pattern.type} {prio_pattern.name}")
                         del match
                         return
             finally:
@@ -104,7 +116,7 @@ class Story:
                         x = match_locations[-1][0] + w / 2 + self.bounding_box.get('left')
                         y = match_locations[-1][1] + h / 2 + self.bounding_box.get('top')
                         self.mouse.set_position_and_left_click(x, y)
-                        # print(f"{max_val} click {_pattern.type} {_pattern.name}")
+                        print(f"{max_val} click {_pattern.type} {_pattern.name}")
                         await asyncio.sleep(0.1)
                         del match
                         return
@@ -113,23 +125,25 @@ class Story:
         # find quest
         await self.find_quest()
 
+    # threshold main quest: 0.80
     async def find_quest(self):
         _timer = time.time()
         while time.time() - _timer < 1:
+            _sct_img = await self.get_quest_screenshot()
             for _pattern in self.patterns_quest:
                 try:
                     w, h = _pattern.img.shape[::-1]
                     match = cv2.matchTemplate(
-                        self.sct_img, _pattern.img, cv2.TM_CCOEFF_NORMED
+                        _sct_img, _pattern.img, cv2.TM_CCOEFF_NORMED
                     )
                     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(match)
                     print(f"{_pattern.type}{_pattern.name} min_val{min_val}, max_val{max_val}, min_loc{min_loc}, max_loc{max_loc}")
-                    if max_val >= 0.48:
+                    if max_val >= 0.92:
                         match_locations = [max_loc]
                         if numpy.asarray(match_locations).size != 0:
-                            x = match_locations[-1][0] + (w - 1) + self.bounding_box.get('left')
-                            y = match_locations[-1][1] + (h - 1) + self.bounding_box.get('top')
-                            print(f"{max_val} click {max_loc} {_pattern.type} {_pattern.name}")
+                            x = match_locations[-1][0] + (w - 1) + self.quest_bounding_box.get('left')
+                            y = match_locations[-1][1] + (h - 1) + self.quest_bounding_box.get('top')
+                            print(f"{max_val} click {max_loc} {_pattern.type} {_pattern.name} ({x},{y})")
                             self.mouse.set_position_and_left_click(x, y)
                             await asyncio.sleep(0.1)
                             del match
